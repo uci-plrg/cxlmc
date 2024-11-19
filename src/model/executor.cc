63b2605844df9ead9eb2b54c569d8cb7ff61cc21
Conan Truong <cjtruong@uci.edu>
2024-11-19 15:29:32 -0800

    implemented mutex types correctly

--- src/model/executor.cc
+++ src/model/executor.cc
@@ -84,15 +84,17 @@ void execute(ModelAction* action) {
 
         if (!owner) {
             mutex->set_owner(curr_thread);
-            mutex->increment_lock_count();
             break;
         }
 
         if (curr_thread == owner) {
             if (mutex->get_mutex_type() == PTHREAD_MUTEX_RECURSIVE) {
                 mutex->increment_lock_count();
-            } else {
+            } else if (mutex->get_mutex_type() == PTHREAD_MUTEX_ERRORCHECK) {
                 errno = EDEADLK;
+            } else {
+                printf("DEADLOCK\n");
+                abort();
             }
             break;
         }
@@ -102,7 +104,6 @@ void execute(ModelAction* action) {
 
         assert(!mutex->get_owner());
         mutex->set_owner(curr_thread);
-        mutex->increment_lock_count();
         break;
     }
     case ATOMIC_WAIT:
@@ -117,7 +118,8 @@ void execute(ModelAction* action) {
             break;
         }
 
-        if (!mutex->decrement_lock_count()) {
+        if (mutex->get_mutex_type() == PTHREAD_MUTEX_RECURSIVE && mutex->get_recursive_lock_count() > 0) {
+            mutex->decrement_lock_count();
             break;
         }
 
